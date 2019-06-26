b654a898c1848680b495e602c6852ee679ab1a50
BananaMcFruit <50183967+BananaMcFruit@users.noreply.github.com>
2019-06-26 17:45:24 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float Req (Re_s, Re_p);
+int R1, int R2:
+int main()
+{
+    int opcion
+ printf("Desea realizar circuito en Serie [1] o circuito en Paralelo [2]?");
+scanf("%d", opcion);
+
+switch(Req);{
+   case 1:
+    printf("Digite el valor R1");
+    scanf("%f", R1);
+    printf("Digite el valor R2");
+    scanf("%f", R2);
+
+     Re_s = (R1+R2);
+    return Re_s;
+    break:
+    }
+
+  {
+   case 2:
+    printf("Digite el valor R1");
+    scanf("%f", R1);
+    printf("Digite el valor R2");
+    scanf("%f", R2);
+
+    Re_p = (R1*R2)/(R1+R2);
+    return Re_p;
+    break:
+    }
+}
